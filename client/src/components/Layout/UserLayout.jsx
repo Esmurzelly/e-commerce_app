23c7cf792564f7d0b9e03575ee127c98b2b6a111
Dark Lord <adamsuper148@gmail.com>
2025-05-05 17:31:59 +0300

    Header / Cart / Footer

--- client/src/components/Layout/UserLayout.jsx
+++ client/src/components/Layout/UserLayout.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from '../Common/Header'
+import Footer from '../Common/Footer'
 
 const UserLayout = () => {
   return (
-    <>
+    <div className='flex flex-col min-h-screen'>
         <Header />
-    </>
+
+        <main className='flex-1'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, alias.
+        </main>
+
+      <Footer />
+    </div>
   )
 }
 
